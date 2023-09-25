af38adcdfc6e9f39f2f801f28a5e8f1bf0ee5676
guohaobin <haobin.guo@ux168.cn>
2023-09-25 18:40:25 +0800

    wip: learn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
-const { BACKEND_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+import tailwind from "@astrojs/tailwind";
+const {
+  BACKEND_URL
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+console.log(BACKEND_URL);
 
-console.log(BACKEND_URL)
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
